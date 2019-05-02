f77e0880b4872c86e199da469934c44bbd05b4d4
Sam <juntilla@hawaii.edu>
2019-05-01 21:23:14 -1000

    Please enter the commit message for your changes. Lines starting

--- pages/items.js
+++ pages/items.js
@@ -1,6 +1,9 @@
+import Header from './components/header';
+
 export default function Items() {
     return (
         <div>
+            <Header />
             <p>THIS IS WHERE OUR ITEMS WILL PROBABLY GO</p>
         </div>
     )
