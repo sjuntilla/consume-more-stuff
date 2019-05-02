5e19224a45d0b9fb4684f35043e3386923480207
Sam <juntilla@hawaii.edu>
2019-05-01 20:35:04 -1000

    creates index.js per next.js example

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "403b8f23aa56eb2883ba";
+/******/ 	var hotCurrentHash = "25b47816b3dae79a337e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
