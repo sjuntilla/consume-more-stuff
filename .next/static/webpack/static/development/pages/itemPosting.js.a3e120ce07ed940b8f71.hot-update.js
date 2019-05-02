53b1db712bf87b9e7b25f246de841beac159c10e
Sam <juntilla@hawaii.edu>
2019-05-01 22:00:44 -1000

    experiments with withRouter

---
+++ .next/static/webpack/static/development/pages/itemPosting.js.a3e120ce07ed940b8f71.hot-update.js
@@ -0,0 +1,54 @@
+webpackHotUpdate("static/development/pages/itemPosting.js",{
+
+/***/ "./pages/itemPosting.js":
+/*!******************************!*\
+  !*** ./pages/itemPosting.js ***!
+  \******************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/router */ "./node_modules/next/dist/client/router.js");
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_myLayout__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/myLayout */ "./pages/components/myLayout.js");
+var _jsxFileName = "/Users/sjuntilla/Documents/devleague/consume-more-stuff/pages/itemPosting.js";
+
+// import { withRouter } from 'next/router'
+// import Layout from './components/myLayout.js'
+// const Page = withRouter(props => (
+//     <Layout>
+//         <h1>{props.router.query.title}</h1>
+//         <p>This is the item post content.</p>
+//     </Layout>
+// ))
+// export default Page
+
+
+
+const Content = props => react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 18
+  },
+  __self: undefined
+}, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 18
+  },
+  __self: undefined
+}, props.router.query.title), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 19
+  },
+  __self: undefined
+}, "holy shit man"));
+
+/***/ })
+
+})
+//# sourceMappingURL=itemPosting.js.a3e120ce07ed940b8f71.hot-update.js.map
