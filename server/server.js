3c7e3cb65976af3a5be8194b898f9f018eeb6bcf
Djavan Cackley <djavancackley@Djavans-MBP.home>
2019-05-01 21:11:32 -1000

    updated

--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const
 
 const PORT = process.env.PORT;
-const REDIS_HOSTNAME = process.env.REDUS_HOSTNAME;
+const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME;
 
 if (!PORT) { console.log('No Port Found'); }
 
 app.use(bodyParser.json());
 
+// start server
 app.listen(PORT, () => {
     console.log(`Server stated on port: ${PORT}`);
 });
