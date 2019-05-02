65aa1939b57d2fcf694d146348d4af98f2a29fe4
Djavan Cackley <djavancackley@Djavans-MBP.home>
2019-05-01 21:59:52 -1000

    changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const
+const decorator = require('./database/decorator')
 
 const PORT = process.env.PORT;
 const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME;
