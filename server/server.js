588a0f20f5d090dc8cc457221c72a4f9745a91cd
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-Air.home>
2019-05-02 20:27:14 -1000

    added decorator to server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const userRoutes = require("./routes/users");
+const itemRoutes = require("./routes/items");
+const decorator = require("./database/decorator");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME;
 
 if (!PORT) {
-    console.log("No Port Found");
+  console.log("No Port Found");
 }
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ extended: true }));
+app.use(decorator);
+
+app.get("/api/smoke", (req, res) => {
+  res.json({ smoke: "test" });
+});
 
 // start server
 app.listen(PORT, () => {
-    console.log(`Server stated on port: ${PORT}`);
+  console.log(`Server stated on port: ${PORT}`);
 });
